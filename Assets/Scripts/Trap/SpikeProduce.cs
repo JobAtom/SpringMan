7eb9972565ec99c49ebb2d0f2904bb28de3b96ad
Aobo Jin <jinaobo-cid@163.com>
2015-02-17 21:47:21 -0600

    Set the wall layer to Wall add a spike trap
    As I said  change the wall to Wall layer which is easy to control

---
+++ Assets/Scripts/Trap/SpikeProduce.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeProduce : MonoBehaviour {
+	public GameObject spike;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find ("spike(Clone)") == null) 
+		{
+			Instantiate (spike,this.gameObject.transform.position,Quaternion.identity);
+		}
+	
+	}
+}
