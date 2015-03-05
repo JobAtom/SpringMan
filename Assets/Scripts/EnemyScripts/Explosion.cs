88d294e7a691bc07a50ea6e5d849ce745f6a3881
arcanearcher <wujk.hust2009@gmail.com>
2015-03-05 13:50:51 -0600

    water mine works

---
+++ Assets/Scripts/EnemyScripts/Explosion.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	float lastingtime = 0.7f;
+	void Start () {
+		//Rigidbody2D body = GetComponent<Rigidbody2D>();
+		//body.AddForce(new Vector2(0, 20000f));
+		//body.AddTorque(Random.Range(-100000f, 100000f));
+		Invoke("DestroySelf",lastingtime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			other.gameObject.GetComponent<HeroController>().particle.Emit (1);
+			if(other.gameObject.GetComponent<HeroController >().Vitals.TakeDamage ())
+			{
+				var shield = GameObject.Find("SpikeShield").gameObject;
+				shield.GetComponent<SpikeShieldScript>().Drop();
+			}
+
+
+		}
+	}
+
+	void DestroySelf()
+	{
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/EnemyScripts/MineScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineScript : MonoBehaviour {
+
+
+	public GameObject ExplosionEffect;
+	public float delay;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.collider.tag == "Player" || other.collider.tag == "trap") 
+		{
+			Invoke ("Explosion",delay);
+			//Debug.Log("Explosion");
+
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if (other.collider.tag == "Player" || other.collider.tag == "trap") 
+		{
+			Invoke ("Explosion",delay);
+			//Debug.Log("Explosion");
+			
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" || other.tag == "trap") 
+		{
+			Invoke ("Explosion",delay);
+			Debug.Log("Explosion");
+			
+		}
+	}
+
+
+
+	void Explosion()
+	{
+		CancelInvoke ();
+		Instantiate (ExplosionEffect, new Vector3 (transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler (new Vector3 (0, 0, 0)));
+		Destroy(this.gameObject);
+	}
+
+}
